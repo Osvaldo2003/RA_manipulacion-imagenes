f8d9344c9cd27f43be2fb07e19b35ab36e7a8dfc
Osvaldo2003 <221258@ids.upchiapas.edu.mx>
2025-07-09 22:45:51 -0600

    ajuste d ecolores

--- app.py
+++ app.py
@@ -17,6 +17,30 @@ def load_image(image_path):
     img = cv2.imread(image_folder + image_path)
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+# Función para mostrar la imagen cargada
+def display_image(image_path=None, image=None):
+    if image_path:
+        img = load_image(image_path)
+    elif image is not None:
+        img = image
+    return img
+
+# Función para convertir entre espacios de color
+def convert_color(image, color_space):
+    if color_space == "BGR to RGB":
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    elif color_space == "RGB to BGR":
+        return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    elif color_space == "RGB to HSV":
+        return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    elif color_space == "HSV to RGB":
+        return cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+    elif color_space == "RGB to LAB":
+        return cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
+    elif color_space == "LAB to RGB":
+        return cv2.cvtColor(image, cv2.COLOR_LAB2RGB)
+    return image
+
 def process_image(image, rotation=0, flip=0, resize_percentage_width=100, resize_percentage_height=100, brightness=0, contrast=1, filter_type="Mediana", gamma=1.0):
     # Aplicar rotación
     rows, cols = image.shape[:2]
@@ -48,9 +72,11 @@ def process_image(image, rotation=0, flip=0, resize_percentage_width=100, resize
     elif filter_type == "Bilateral":
         filtered = cv2.bilateralFilter(gamma_corrected, 9, 75, 75)
     elif filter_type == "Detección de bordes":
-        # Convertir la imagen a escala de grises antes de aplicar Canny
-        gray_image = cv2.cvtColor(gamma_corrected, cv2.COLOR_RGB2GRAY)
-        filtered = cv2.Canny(gray_image, 100, 200)  # Aplicar detección de bordes con Canny
+        # Detección de bordes con Canny
+        gray = cv2.cvtColor(gamma_corrected, cv2.COLOR_RGB2GRAY)
+        filtered = cv2.Canny(gray, 100, 200)
+        # Convertir la imagen a RGB para evitar problemas de canales al combinarla con el fondo
+        filtered = cv2.cvtColor(filtered, cv2.COLOR_GRAY2RGB)
     elif filter_type == "Enfocar":
         kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
         filtered = cv2.filter2D(gamma_corrected, -1, kernel)
@@ -81,11 +107,8 @@ def process_image(image, rotation=0, flip=0, resize_percentage_width=100, resize
     
     # Ecualización de Histograma
     if filter_type == "Ecualización":
-        # Convertir la imagen a escala de grises antes de ecualizar
         gray_image = cv2.cvtColor(gamma_corrected, cv2.COLOR_RGB2GRAY)
         filtered = cv2.equalizeHist(gray_image)
-        # Después de la ecualización, convertir la imagen de nuevo a color si es necesario
-        filtered = cv2.cvtColor(filtered, cv2.COLOR_GRAY2RGB)
 
     # Mejoramiento de detalles (realce)
     if filter_type == "Realce":
@@ -94,21 +117,10 @@ def process_image(image, rotation=0, flip=0, resize_percentage_width=100, resize
 
     return filtered
 
-# Función para mostrar la imagen cargada
-def display_image(image_path=None, image=None):
-    if image_path:
-        img = load_image(image_path)
-    elif image is not None:
-        img = image
-    return img
-
 # Función para guardar la imagen procesada en la carpeta local con nombre único
 def save_image(image, filename="processed_image.jpg"):
-    # Generar un nombre único para cada descarga
     timestamp = time.strftime("%Y%m%d_%H%M%S")
     output_path = os.path.join("descargas", f"{timestamp}_{filename}")
-    
-    # Guardar la imagen procesada
     cv2.imwrite(output_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
     return output_path
 
@@ -121,18 +133,14 @@ def combine_with_background(processed_image, background_image_path):
 
 # Función para descargar la imagen combinada en un solo collage
 def download_images(original_image, processed_image, background_image):
-    # Crear un collage con las tres imágenes
     processed_resized = cv2.resize(processed_image, (original_image.shape[1], original_image.shape[0]))
-    
-    # Solo aplicar fondo a la imagen combinada
     combined_image = combine_with_background(processed_image, background_image)
     
-    # Crear collage final con las tres imágenes (original, transformada, combinada con fondo)
+    # Crear collage final con las tres imágenes
     collage = np.hstack((original_image, processed_resized, combined_image))
     
     # Guardar la imagen combinada (collage) con un nombre único
     output_path = save_image(collage, "collage_imagenes.jpg")
-    
     return output_path
 
 # Interfaz de Gradio
@@ -152,6 +160,9 @@ with gr.Blocks() as demo:
         contrast_slider = gr.Slider(1, 3, step=0.1, label="Contraste de la imagen")
         filter_dropdown = gr.Dropdown(choices=["Imagen Original", "Mediana", "Gaussiano", "Bilateral", "Detección de bordes", "Enfocar", "Media", "Sobel", "Scharr", "Dilatación", "Erosión", "Contornos", "Umbral", "Ecualización", "Realce"], label="Selecciona un filtro")
         gamma_slider = gr.Slider(0.1, 3.0, step=0.1, label="Corrección Gamma")
+        
+        # Nuevo dropdown para selección de espacio de color
+        color_space_dropdown = gr.Dropdown(choices=["Ninguno", "BGR to RGB", "RGB to BGR", "RGB to HSV", "HSV to RGB", "RGB to LAB", "LAB to RGB"], label="Selecciona un espacio de color", value="Ninguno")
 
     with gr.Row():
         # Mostrar la imagen transformada antes de combinarla
@@ -168,17 +179,21 @@ with gr.Blocks() as demo:
     with gr.Row():
         # Botón para descargar las imágenes (original + procesada)
         download_button = gr.Button("Descargar Imágenes")
-        download_output = gr.File(label="Descargar Imágenes", visible=False)  # Usamos el componente gr.File para la descarga
+        download_output = gr.File(label="Descargar Imágenes", visible=False)
 
     # Lógica para cargar la imagen y aplicar transformaciones
-    def apply_transformations(image_path, rotation, flip, resize_percentage_width, resize_percentage_height, brightness, contrast, filter_type, gamma, background_image):
+    def apply_transformations(image_path, rotation, flip, resize_percentage_width, resize_percentage_height, brightness, contrast, filter_type, gamma, background_image, color_space):
         image = load_image(image_path)
         
         # Si se selecciona "Imagen Original", no aplicar ninguna transformación
-        if filter_type == "Imagen Original":
-            transformed_image = image
-        else:
+        if filter_type != "Imagen Original":
             transformed_image = process_image(image, rotation, flip, resize_percentage_width, resize_percentage_height, brightness, contrast, filter_type, gamma)
+        else:
+            transformed_image = image
+        
+        # Aplicar la conversión de color automáticamente, si el usuario elige transformaciones que lo requieran
+        if color_space != "Ninguno":
+            transformed_image = convert_color(transformed_image, color_space) 
         
         # Si se selecciona un fondo, combinarlo con la imagen transformada
         if background_image:
@@ -186,9 +201,9 @@ with gr.Blocks() as demo:
         else:
             combined_image = transformed_image
         
-        return image, transformed_image, combined_image  # Devolver la imagen original, la transformada y la combinada
+        return image, transformed_image, combined_image
 
-    # Lógica para descarga de ambas imágenes (incluyendo collage)
+    # Lógica para descarga de las imágenes (incluyendo collage)
     def download_file(original_image, processed_image, background_image):
         # Crear collage y devolver el archivo guardado
         saved_image_path = download_images(original_image, processed_image, background_image)
@@ -197,28 +212,28 @@ with gr.Blocks() as demo:
     # Enlazar los eventos
     image_input.change(display_image, inputs=image_input, outputs=image_display)
     rotation_slider.change(apply_transformations, 
-                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                            outputs=[image_display, transformed_image_display, combined_image_display])
     flip_slider.change(apply_transformations, 
-                       inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                       inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                        outputs=[image_display, transformed_image_display, combined_image_display])
     resize_percentage_width_slider.change(apply_transformations, 
-                                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                                            outputs=[image_display, transformed_image_display, combined_image_display])
     resize_percentage_height_slider.change(apply_transformations, 
-                                            inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                                            inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                                             outputs=[image_display, transformed_image_display, combined_image_display])
     brightness_slider.change(apply_transformations, 
-                             inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                             inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                              outputs=[image_display, transformed_image_display, combined_image_display])
     contrast_slider.change(apply_transformations, 
-                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                            outputs=[image_display, transformed_image_display, combined_image_display])
     filter_dropdown.change(apply_transformations, 
-                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                           inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                            outputs=[image_display, transformed_image_display, combined_image_display])
     gamma_slider.change(apply_transformations, 
-                        inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown], 
+                        inputs=[image_input, rotation_slider, flip_slider, resize_percentage_width_slider, resize_percentage_height_slider, brightness_slider, contrast_slider, filter_dropdown, gamma_slider, background_dropdown, color_space_dropdown], 
                         outputs=[image_display, transformed_image_display, combined_image_display])
     download_button.click(download_file, inputs=[image_display, transformed_image_display, background_dropdown], outputs=download_output)
 
